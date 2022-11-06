74d426d9d5c931fdc0300b15ca758bb3d18c0e8e
Lua_Developer <83262616+Lua-developer@users.noreply.github.com>
2022-11-06 16:19:55 +0900

    Update 2022.11.06

--- module/DB.py
+++ module/DB.py
@@ -4,11 +4,21 @@ DB.py
 '''
 # mySQL connect
 # 데이터베이스의 연결 정보를 저장
+# sql_connection 클래스를 이용하여 conn, cursor로 관리
+# 생성자 단위에서 connection, cursor 생성
 import pymysql
-def con_and_make_cursor():
-    conn = pymysql.connect(host='localhost',
-                       user='root', 
-                       password='admin',
-                       db='parking_service', 
-                       charset='utf8')    
-    return conn
+
+class sql_connection :
+    def __init__(self) :
+        self.conn = self.connection()   
+        self.cursor = self.make_cursor(self.conn)
+    def connection(self) :
+        conn = pymysql.connect(host='localhost',
+                        user='root', 
+                        password='자기 비밀번호',
+                        db='parking_service', 
+                        charset='utf8')   
+        return conn
+    def make_cursor(self, conn):
+        cur = conn.cursor()
+        return cur
