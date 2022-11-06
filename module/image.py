2cf838b1ae8e235ebb3e35e509d40ec9339c80dd
Lua_Developer <83262616+Lua-developer@users.noreply.github.com>
2022-11-06 16:21:16 +0900

    Update image.py 2022_11_06
    모델 입력에 필요한 이미지 전처리 함수 추가

--- module/image.py
+++ module/image.py
@@ -28,4 +28,15 @@ class processimg :
         file_name = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
         cv2.imwrite("Capture/"+ file_name + ".png", img)
         return file_name
-    
+    # 모델 입력용 이미지 전처리 함수
+    def machine_preprocessing(self, img) :
+        # 객체 사이즈 축소 및 보간
+        frame_resized = cv2.resize(img, [224,224], interpolation=cv2.INTER_AREA)
+        # 이미지 정규화
+        frame_normalized = (frame_resized.astype(np.float32) / 127.0) - 1
+
+        # 예측을 위해 reshape.
+        # 학습한 이미지와 사이즈가 동일하게 만든다.
+        frame_reshaped = frame_normalized.reshape((1, 224, 224, 3))
+        # 객체 반환
+        self.img = frame_reshaped
